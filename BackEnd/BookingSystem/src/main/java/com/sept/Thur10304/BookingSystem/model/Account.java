23bf0b6fc0177fa7425b055118f40b66d756f305
Ryan Babij <rbabij@gmail.com>
2020-08-18 18:38:22 +1000

    Validation and errors
    * Add input validation
    * Add account creation date field
    * Add error messages for invalid field input

--- BackEnd/BookingSystem/src/main/java/com/sept/Thur10304/BookingSystem/model/Account.java
+++ BackEnd/BookingSystem/src/main/java/com/sept/Thur10304/BookingSystem/model/Account.java
@@ -1,24 +1,32 @@
 package com.sept.Thur10304.BookingSystem.model;
 
 import javax.persistence.*;
-// import javax.validation.constraints.NotBlank;
-// import javax.validation.constraints.Size;
+import javax.validation.constraints.NotBlank; // require non-null fields
+import javax.validation.constraints.Size; // require fields of min, max length
+
 import com.fasterxml.jackson.annotation.*;
-import java.util.Date;
+
+import ch.qos.logback.core.joran.conditional.ElseAction;
+
+import java.util.Date; // for registration date
 
 @Entity
 public class Account {
 
-    // Still needs more variables (email,etc)
-    // Also needs validation
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Size(min=3, max=20, message = "Username must be between 3-20 characters.")
+    @NotBlank(message = "Username is required.")
     private String username;
+    @Size(min=6, max=20, message = "Password must be between 6-20 characters.")
+    @NotBlank(message = "Password is required.")
     private String password;
+    @NotBlank(message = "Email is required.")
+    @Size(min=3, max=320, message = "Email must be between 3-320 characters.")
     private String email;
-
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    private Date dateCreated;
 
 
     public Long getId() {
@@ -26,8 +34,6 @@ public class Account {
     }
 
     public void setId(Long id) {
-
-        // validation
         this.id = id;
     }
 
@@ -36,8 +42,6 @@ public class Account {
     }
 
     public void setUsername(String username) {
-
-        // validation
         this.username = username;
     }
 
@@ -46,11 +50,9 @@ public class Account {
     }
 
     public void setPassword(String password) {
-        
-        // validation
         this.password = password;
     }
-    
+
     public String getEmail()
     {
         return this.email;
@@ -58,10 +60,22 @@ public class Account {
 
     public void setEmail(String email)
     {
-        
-        // validation
-        this.email = email;
+        this.email=email;
     }
 
+    public Date getDateCreated()
+    {
+        return this.dateCreated;
+    }
+    public void setDateCreated(Date dateCreated)
+    {
+        this.dateCreated = dateCreated;
+    }
+
+    @PrePersist
+    protected void onCreate()
+    {
+        this.dateCreated = new Date();
+    }
 
 }
--- BackEnd/BookingSystem/src/main/java/com/sept/Thur10304/BookingSystem/web/AccountController.java
+++ BackEnd/BookingSystem/src/main/java/com/sept/Thur10304/BookingSystem/web/AccountController.java
@@ -10,8 +10,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.FieldError; // validation errors
 
-import javax.validation.Valid;
+import javax.validation.Valid; // field validation
+import java.util.List;
+import java.util.Map; // error map.
+import java.util.HashMap; // error map
 
 @RestController
 @RequestMapping("/api/Account")
@@ -23,7 +27,13 @@ public class AccountController {
     @PostMapping("")
     public ResponseEntity<?> createNewAccount(@Valid @RequestBody Account account, BindingResult result) {
         if (result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Account Object", HttpStatus.BAD_REQUEST);
+            //Map <String, String> errorMap = new HashMap<>();
+
+            //for (FieldError error : result.getFieldErrors()){
+                return new ResponseEntity <List<FieldError>>(result.getFieldErrors(), HttpStatus.BAD_REQUEST);
+            //}
+
+            //return new ResponseEntity<String>("Invalid Account Object", HttpStatus.BAD_REQUEST);
         }
         Account account1 = accountService.saveOrUpdateAccount(account);
         return new ResponseEntity<Account>(account, HttpStatus.CREATED);
