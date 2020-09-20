1a451d0dde46220ad970cb32c369b9c5b5f48081
lukedavoli <ldavoli.mail@gmail.com>
2020-09-20 16:32:17 +1000

    Worker and Admin testing

---
+++ BackEnd/BookingSystem/src/test/java/com/sept/Thur10304/BookingSystem/web/Account_Controller_CreateAdminTest.java
@@ -0,0 +1,234 @@
+package com.sept.Thur10304.BookingSystem.web;
+
+import com.sept.Thur10304.BookingSystem.repositories.AccountRepository;
+import com.sept.Thur10304.BookingSystem.repositories.AdminRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class Account_Controller_CreateAdminTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void createNewAdmin_accepted() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void createNewAdmin_firstNameTooShort() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_firstNameTooLong() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ronald Duke Silver Tammy I and Tammy II\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_firstNameBlank() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_lastNameTooShort() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_lastNameTooLong() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson, Pawnee's Resident Libertarian\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_lastNameBlank() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_passwordTooShort() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"Tammy\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_passwordTooLong() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilverandIWouldLikeAllYourEggsPlease\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_passwordBlank() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"email\": \"ron@pawnee.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_emailTooShort() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"r@\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_emailTooLong() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"ron@pawnefjhberifgbsaiffvbcaswfncvawsibnvasZPfvncwsiaufcvnbsaufchnsjufchnsdfchnSJDCvhnsdujfvhbcnsdafjgvbldfazsikjguvbnsiyfvgblsdfvjubnasdfvgbsdafyhkvbzdlfivbnzdsfiyuvbsdfjvnbzfvjhnsdzfiaugvhnasdrnfgbvisdbfnvszdjufvbnhzsdjufnvbsdifzvbnzisfbnzsduihfncvizdusfhbnvclzsdidhjdhjuehueduedbnfebnfejndfjbdbefdhjefdujhnbvuijhvne.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_emailIncorrectFormat() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"email\": \"r$o%n@paw.nee@.gov\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void createNewAdmin_emailBlank() throws Exception {
+
+        mvc.perform(post("/api/Account/saveadmin")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"firstName\": \"Ron\",\n" +
+                        "    \"lastName\": \"Swanson\",\n" +
+                        "    \"password\": \"IAmDukeSilver\",\n" +
+                        "    \"dateCreated\": \"2020-08-23\"\n" +
+                        "}"))
+                .andExpect(status().isBadRequest());
+    }
+}
